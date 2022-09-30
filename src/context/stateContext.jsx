a8ecc4bd01edda3b329bc3461d8d9f1699d5b3ed
Luis Medina <lamc3005@gmail.com>
2022-09-30 14:05:31 -0400

    added filter and darkMode

--- src/context/stateContext.jsx
+++ src/context/stateContext.jsx
@@ -6,8 +6,9 @@ export const StateContext = createContext()
 export function StateContextProvider({ children }) {
 
   const [state, setState] = useState(JSON.parse(localStorage.getItem('task')) || [])
-
-  const data = { state, setState }
+  const [filter, setFilter] = useState('all')
+  const [darkMode, setDarkMode] = useState(true)
+  const data = { state, setState, filter, setFilter, darkMode, setDarkMode }
 
   return (
     <StateContext.Provider value={data}>
